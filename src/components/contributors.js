a4227c11940745fe9c7fc08f36ffe1c1a623e7c9
Eduardo Correa <eduardocorrea.lecs@gmail.com>
2020-03-22 21:41:49 -0300

    Section about contributors

---
+++ src/components/contributors.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { contributors } from '../services/firebase'
+
+export default function contributors() {
+  return (
+    <div className="text-justify p-5 pl-10 pr-10">
+      <h1 className="text-2xl text-red-500 text-center uppercase mb-5">
+        Veja quem <span className="font-bold">já faz parte!</span>
+      </h1>
+    </div>
+  )
+}
